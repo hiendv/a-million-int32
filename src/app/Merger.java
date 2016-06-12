ee1e8cf1fd7f22e49d525e7caabaf5875a4a45c5
hiendv <neo.go.vn@gmail.com>
2016-06-12 17:04:12 +0700

    Refactor Merger: testable

--- src/app/Merger.java
+++ src/app/Merger.java
@@ -16,11 +16,10 @@
  */
 package app;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.Scanner;
 
 /**
@@ -29,34 +28,31 @@ import java.util.Scanner;
  */
 public class Merger implements app.contracts.Merger {
 
-    protected String input;
-    protected String output;
-    protected int chunkCount;
-    protected int[] heap;
-    int lastPick = -1;
-    int finishedCount = 0;
-    long[] skips;
+    protected File[] files;
+    protected Writer writer;
 
-    public Merger(String input, String output, int chunkCount) {
-        this.input = input;
-        this.output = output;
-        this.chunkCount = chunkCount;
-        this.heap = new int[chunkCount];
-        this.setUpIO();
+    /**
+     * Merge chunks
+     * @param files
+     * @param writer
+     */
+    public Merger(File[] files, Writer writer) {
+        this.files = files;
+        this.writer = writer;
     }
 
-    private void setUpIO() {
-        File outputDirectory = new File(this.output).getParentFile();
-        if (!outputDirectory.exists()) {
-            outputDirectory.mkdirs();
-        }
-    }
-
-    protected String findMin() {
+    /**
+     * Find the smallest integer
+     * @param heap
+     * @param skips
+     * @param lastPick
+     * @return
+     */
+    protected int findMin(int[] heap, long[] skips, int lastPick) {
         if (lastPick == -1 || skips[lastPick] == -1) {
             // lastPick is not set or belongs to a "dead" chunk
             // Get a new one
-            for (int i = 0; i < this.chunkCount; i++) {
+            for (int i = 0; i < heap.length; i++) {
                 if (skips[i] == -1) {
                     continue;
                 }
@@ -68,7 +64,7 @@ public class Merger implements app.contracts.Merger {
 
         int min = heap[lastPick];
 
-        for (int i = 0; i < this.chunkCount; i++) {
+        for (int i = 0; i < heap.length; i++) {
             if (skips[i] == -1) {
                 // "Dead" chunk
                 continue;
@@ -83,20 +79,17 @@ public class Merger implements app.contracts.Merger {
             }
         }
 
-        return min + "\n";
+        return lastPick;
     }
 
     @Override
     public void merge() throws IOException {
-        BufferedWriter outputWriter;
-        outputWriter = new BufferedWriter(
-            new FileWriter(this.output)
-        );
-
-        skips = new long[heap.length];
-
-        while (finishedCount < this.chunkCount) {
-            for (int i = 0; i < this.chunkCount; i++) {
+        int[] heap = new int[this.files.length];
+        long[] skips = new long[this.files.length];
+        int finishedCount = 0;
+        int lastPick = -1;
+        while (finishedCount < heap.length) {
+            for (int i = 0; i < this.files.length; i++) {
                 // If the chunk runs out of elements, skip it
                 if (skips[i] == -1) {
                     // "Dead" chunk
@@ -106,12 +99,13 @@ public class Merger implements app.contracts.Merger {
                 // If the last pick is not the index
                 // and not the initial value
                 // and the related chunk is not dead
-                if (lastPick != i && lastPick != -1 && skips[lastPick] != -1) {
+                if (lastPick != i
+                    && lastPick != -1
+                    && skips[lastPick] != -1) {
                     continue;
                 }
 
-                FileInputStream inputStream = new FileInputStream(this.input + "/" + i);
-
+                FileInputStream inputStream = new FileInputStream(files[i]);
                 inputStream.skip(skips[i]);
                 Scanner scanner = new Scanner(inputStream, "UTF-8");
 
@@ -125,20 +119,20 @@ public class Merger implements app.contracts.Merger {
                 // Update the heap at the index `i`
                 heap[i] = Integer.parseInt(scanner.nextLine());
 
-                if (lastPick == -1 && i < this.chunkCount - 1) {
+                if (lastPick == -1 && i < heap.length - 1) {
                     // The heap is not completely loaded
                     continue;
                 }
 
                 // Find the minimum value
-                String minStr = this.findMin();
-
+                lastPick = this.findMin(heap, skips, lastPick);
+                String min = heap[lastPick] + "\n";
                 // Write it out and update the skips[lastPick]
-                outputWriter.write(minStr);
-                skips[lastPick] += minStr.length();
-                outputWriter.flush();
+                writer.write(min);
+                skips[lastPick] += min.length();
+                writer.flush();
             }
         }
-        outputWriter.close();
+        writer.close();
     }
 }
